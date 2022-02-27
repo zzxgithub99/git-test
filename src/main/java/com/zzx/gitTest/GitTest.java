c2bf7297cb6e5850bca626e98a17b7c21bbe82dd
zhangzhixin <a563427442@qq.com>
2022-02-27 23:45:43 +0800

    hot-fix  second  commit

--- src/main/java/com/zzx/gitTest/GitTest.java
+++ src/main/java/com/zzx/gitTest/GitTest.java
@@ -6,5 +6,7 @@ public class GitTest {
         System.out.println("hello");
         System.out.println("hello");
         System.out.println("HOT-FIX 修改代码额");
+        System.out.println("HOT-FIX 修改代码额");
+        System.out.println("HOT-FIX 修改代码额");
     }
 }
